7e971960903eba663cd6fdc51df0074aae52d957
dwm9100b <dale@interbaytechnology.com>
2014-04-23 14:54:06 -0700

    Finished tutorial

---
+++ itgLsCheckbox/itgLsCheckbox/itgLsCheckbox.HTMLClient/Screens/Tests/ViewTest.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewTest.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Test.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Test." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
